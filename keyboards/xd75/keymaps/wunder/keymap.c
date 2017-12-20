ae0837a4bf586bde3432ea918272872272312c85
Wunder Dmitry <i.draenei@gmail.com>
2017-12-20 23:05:18 +0300

    Nice layout

--- keyboards/xd75/keymaps/wunder/keymap.c
+++ keyboards/xd75/keymaps/wunder/keymap.c
@@ -45,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = { /* QWERTY */
-  { KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,    KC_HOME, KC_UP,   KC_PGUP, KC_6,   KC_7,   KC_8,    KC_9,   KC_0,    KC_QUOT},
-  { KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,    KC_BSLS, KC_RBRC, KC_LBRC, KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    KC_BSPC},
+  { KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,    KC_HOME, KC_UP,   KC_PGUP, KC_6,   KC_7,   KC_8,    KC_9,   KC_0,    KC_BSPC},
+  { KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,    KC_BSLS, KC_RBRC, KC_LBRC, KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    KC_QUOT},
   { KC_LCTL, KC_A,    KC_S,   KC_D,    KC_F,   KC_G,    KC_GRV,  KC_EQL,  KC_MINS, KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN, CTL_T(KC_ENT)},
   { KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,    KC_END,  KC_DEL,  KC_PGDN, KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT },
-  { TT(_LW), KC_LGUI, FN_SPC, KC_LALT, FN_SPC, FN_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, FN_SPC, FN_SPC, KC_RALT, FN_SPC, KC_RGUI, TT(_LW)},
+  { TT(_LW), KC_LGUI, FN_SPC, KC_LALT, FN_SPC, FN_SPC,  KC_LEFT, KC_F14, KC_RGHT, FN_SPC, FN_SPC, KC_RALT, FN_SPC, KC_RGUI, TT(_LW)},
  },
 
  [_FN] = { /* FUNCTION */
