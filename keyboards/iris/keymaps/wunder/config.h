093966e1de9637dfad1bedc074cdd0b4634c9b71
Wunder Dmitry <i.draenei@gmail.com>
2018-02-16 23:03:46 +0300

    IRIS BABY

--- keyboards/iris/keymaps/wunder/config.h
+++ keyboards/iris/keymaps/wunder/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Danny Nguyen <danny@hexwire.com>
+Copyright 2017 Danny Nguyen <danny@keeb.io>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,13 +15,30 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define TAPPING_TOGGLE 1
-#define DEBOUNCING_DELAY 5
-#define TAPPING_TERM 100
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
 
 #include "config_common.h"
+#define AUTO_SHIFT_TIMEOUT 200
+#define NO_AUTO_SHIFT_ALPHA
 
-#endif  // CONFIG_H
+/* Use I2C or Serial, not both */
+
+#define USE_SERIAL
+// #define USE_I2C
+
+/* Select hand configuration */
+
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+#endif
--- keyboards/xd75/keymaps/wunder/keymap.c
+++ keyboards/xd75/keymaps/wunder/keymap.c
@@ -20,7 +20,8 @@
 // Fillers to make layering more clear
 #define ______ KC_TRNS 
 #define FN_SPC LT(_FN, KC_SPC)
-#define CTL_GRV CTL_T(KC_GRV)
+#define FN_BSP LT(_FN, KC_BSPC)
+#define CTL_ESC CTL_T(KC_ESC)
 #define SFT_MEH SFT_T(KC_F14)
 #define SFT_MIN MT(MOD_RSFT, KC_MINS)
 #define CTL_ENT MT(MOD_RCTL, KC_ENT)
@@ -48,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = { /* QWERTY */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_HOME, KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,  KC_F14},
+  { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_HOME, KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,  KC_F14},
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_RBRC, KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,  KC_BSLS},
-  { CTL_GRV, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,  KC_MINS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_ENT,  KC_EQL},
+  { CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,  KC_MINS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_ENT,  KC_EQL},
   { SFT_MEH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_MIN,  KC_DEL},
-  { KC_F14,  KC_LGUI, TT(_LW), KC_LALT, FN_SPC,  FN_SPC,  KC_LEFT, KC_RGHT, FN_SPC,  FN_SPC,  KC_RALT, TT(_LW), KC_RGUI, TT(_LW),  KC_DOWN},
+  { KC_PAUS, KC_LGUI, TT(_LW), KC_LALT, FN_BSP,  FN_BSP,  KC_LALT, KC_RALT, FN_SPC,  FN_SPC,  KC_RALT, TT(_LW), KC_RGUI, TT(_LW),  KC_DOWN},
  },
 
  [_FN] = { /* FUNCTION */
@@ -64,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  },
 
  [_LW] = { /* LOWER */
-  { ______,  ______,  ______,  ______,  KC_SLCK, KC_PAUS, ______,  ______,  KC_PSCR, KC_NLCK, KC_PSLS, KC_PAST, KC_P0,   ______,   ______ },
+  { ______,  ______,  ______,  KC_INS,  KC_SLCK, KC_PAUS, ______,  ______,  KC_PSCR, KC_NLCK, KC_PSLS, KC_PAST, KC_P0,   ______,   ______ },
   { KC_CAPS, KC_BTN4, KC_MS_U, KC_BTN5, KC_WH_U, ______,  ______,  ______,  ______,  KC_P7,   KC_P8,   KC_P9,   KC_PMNS, ______,   RESET },
   { ______,  KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, ______,  RGB_TOG, RGB_MOD, ______,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS, CTL_PENT, ______ },
   { ______,  ______,  KC_WH_L, KC_WH_R, KC_BTN3, KC_BTN2, RGB_HUI, RGB_VAI, ______,  KC_P1,   KC_P2,   KC_P3,   KC_PDOT, ______,   RGB_SAI },
