e9a82874e72b1da010dd6d9a514dc037130bb96d
Wunder Dmitry <i.draenei@gmail.com>
2018-02-20 09:21:29 +0300

    baboochkey

--- keyboards/xd75/keymaps/wunder/keymap.c
+++ keyboards/xd75/keymaps/wunder/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    M1_S,    M2_S,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,  KC_BSLS},
   { CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    M1_R,    M2_R,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_ENT,  KC_EQL},
   { SFT_MEH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_MIN,  KC_DEL},
-  { KC_PAUS, KC_LGUI, TT(_LW), KC_LALT, FN_BSP,  FN_BSP,  KC_LALT, KC_RALT, FN_SPC,  FN_SPC,  KC_RALT, TT(_LW), KC_RGUI, TT(_LW),  KC_DOWN},
+  { KC_PAUS, KC_LGUI, TT(_LW), TT(_LW), FN_BSP,  KC_LALT, KC_LALT, KC_RALT, KC_RALT, FN_SPC,  TT(_LW), TT(_LW), KC_RGUI, TT(_LW),  KC_DOWN},
  },
 
  [_FN] = { /* FUNCTION */
