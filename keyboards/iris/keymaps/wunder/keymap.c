f6bae956868fcebfd138233143f303b5c64da5d4
Wunder Dmitry <i.draenei@gmail.com>
2018-08-14 23:18:18 +0300

    iris

--- keyboards/iris/keymaps/wunder/keymap.c
+++ keyboards/iris/keymaps/wunder/keymap.c
@@ -1,96 +1,86 @@
-#include "iris.h"
-#include "action_layer.h"
-#include "eeconfig.h"
+#include QMK_KEYBOARD_H
+
+extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
 #define _LW 1
 #define _FN 2
 
 enum custom_keycodes {
-    KC_M1 = SAFE_RANGE,
-    KC_M2,
-    KC_M3,
-    KC_M4
+    QWERTY = SAFE_RANGE,
+    M1,
+    M2,
+    M3,
+    M4,
+    M5,
+    DYNAMIC_MACRO_RANGE
 };
 
 
-enum planck_keycodes {
-  QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
-  PLOVER,
-  LOWER,
-  RAISE,
-  BACKLIT,
-  EXT_PLV,
-  DYNAMIC_MACRO_RANGE,
-};
+/* enum planck_keycodes { */
+/* }; */
 #include "dynamic_macro.h"
 
-#define KC_M1_S DYN_REC_START1
-#define KC_M2_S DYN_REC_START2
-#define KC_M1_R DYN_MACRO_PLAY1
-#define KC_M2_R DYN_MACRO_PLAY2
-#define KC_M_STOP DYN_REC_STOP
+#define M1_S DYN_REC_START1
+#define M2_S DYN_REC_START2
+#define M1_R DYN_MACRO_PLAY1
+#define M2_R DYN_MACRO_PLAY2
+#define M_STOP DYN_REC_STOP
 
-#define KC_ KC_TRNS
-#define _______ KC_TRNS
-#define KC_RST RESET
-
-#define KC_FN_SPC LT(_FN, KC_SPC)
-#define KC_FN_BSP LT(_FN, KC_BSPC)
-#define KC_CTL_ESC CTL_T(KC_ESC)
-#define KC_FN_ESC LT(_FN, KC_ESC)
-#define KC_SFT_MEH SFT_T(KC_F14)
-#define KC_SFT_MIN MT(MOD_RSFT, KC_MINS)
-#define KC_CTL_ENT MT(MOD_RCTL, KC_ENT)
-#define KC_CTL_PENT MT(MOD_RCTL, KC_PENT)
-#define KC_LW TT(_LW)
-#define KC_FGUI GUI_T(KC_F2)
+#define FN_SPC LT(_FN, KC_SPC)
+#define FN_BSP LT(_FN, KC_BSPC)
+#define CTL_ESC CTL_T(KC_ESC)
+#define FN_ESC LT(_FN, KC_ESC)
+#define SFT_MEH SFT_T(KC_F14)
+#define SFT_MIN MT(MOD_RSFT, KC_MINS)
+#define CTL_ENT MT(MOD_RCTL, KC_ENT)
+#define CTL_PENT MT(MOD_RCTL, KC_PENT)
+#define LW TT(_LW)
+#define FGUI GUI_T(KC_F2)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = KC_KEYMAP(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     GRV , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,QUOT,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     CTL_ESC , A  , S  , D  , F  , G  ,            H  , J  , K  , L  ,SCLN,CTL_ENT,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     SFT_MEH, Z  , X  , C  , V  , B  ,LALT,  RALT, N  , M  ,COMM,DOT ,SLSH,SFT_MIN,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                      LGUI,FN_BSP,LW,          LW,FN_SPC,FGUI
-  //                  `----+----+----'        `----+----+----'
+  [_QWERTY] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_ENT,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     SFT_MEH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,          KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_MIN,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    KC_LGUI, FN_BSP,   LW,                       LW,      FN_SPC,  FGUI
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_FN] = KC_KEYMAP(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-      F1 , F2 , F3 , F4 , F5 , F6,                 F7 , F8 , F9 ,F10 ,F11 ,F12 ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,M1_S,M2_S,M1_R,M2_R,M_STOP,             HOME, END,LBRC,RBRC,    ,DEL ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,               LEFT,DOWN, UP ,RGHT,BSLS,    ,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-         , M1 , M2 , M3 , M4 ,    ,    ,         ,PGUP,PGDN, EQL ,    ,    ,    ,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           ,    ,    ,             ,    ,
-  //                  `----+----+----'        `----+----+----'
+  [_FN] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, M1_S,    M2_S,    M1_R,    M2_R,    M_STOP,                             KC_HOME, KC_END,  KC_LBRC, KC_RBRC, _______, KC_DEL,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MPLY,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSLS, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, M1,      M2,      M3,      M4,      M5,      _______,          _______, KC_PGUP, KC_PGDN, KC_EQL,  _______, _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, _______, _______
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_LW] = KC_KEYMAP(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-         ,    ,    ,INS ,SLCK,PAUS,               PSCR,NLCK,PSLS,PAST, P0 ,    ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     CAPS,BTN4,MS_U,BTN5,WH_U,    ,                   , P7 , P8 , P9 ,PMNS,    ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,MS_L,MS_D,MS_R,WH_D,    ,                   , P4 , P5 , P6 ,PPLS,CTL_PENT,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-         ,    ,WH_L,WH_R,BTN3,BTN2,    ,         ,BTN1, P1 , P2 , P3 ,PDOT,    ,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                            ,    ,    ,           ,    ,RST
-  //                  `----+----+----'        `----+----+----'
+  [_LW] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, KC_INS,  KC_SLCK, KC_PAUS,                            KC_PSCR, KC_NLCK, KC_PSLS, KC_PAST, KC_P0,   _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_CAPS, KC_BTN4, KC_MS_U, KC_BTN5, KC_WH_U, _______,                            _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______,                            _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PENT,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, KC_WH_L, KC_WH_R, KC_BTN3, KC_BTN2, _______,          _______, KC_BTN1, KC_P1,   KC_P2,   KC_P3,   KC_PDOT, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, _______, _______
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 };
 
@@ -101,17 +91,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   if (record->event.pressed) {
     switch(keycode) {
-      case KC_M1:
+      case M1:
         SEND_STRING("gotogit"SS_TAP(X_ENTER));
         return false; break;
-      case KC_M2:
+      case M2:
         SEND_STRING("1234Aa");
         return false; break;
-      case KC_M3:
+      case M3:
         SEND_STRING("3");
         return false; break;
-      case KC_M4:
+      case M4:
         SEND_STRING(SS_LSFT("ZQ"));
+        return false; break;
+      case M5:
+        SEND_STRING(SS_LSFT("ZZ"));
         return false; break;
     }
   }
