89430f9ca75e44d4a622db7f2f15891f1d6d0129
Wunder Dmitry <i.draenei@gmail.com>
2018-11-18 17:38:10 +0300

    game mode with more controls

--- keyboards/iris/keymaps/wunder/config.h
+++ keyboards/iris/keymaps/wunder/config.h
@@ -31,8 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 //#define MASTER_LEFT
-#define MASTER_RIGHT
-// #define EE_HANDS
+// #define MASTER_RIGHT
+#define EE_HANDS
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
