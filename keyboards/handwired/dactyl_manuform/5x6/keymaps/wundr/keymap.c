ae08aa9a162ab44a295ce04387cd70c367f823f5
Dmitry Shashkov <shash@mera.ru>
2019-08-01 16:46:34 +0300

    dactyl 40 is on the way bebe

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/wundr/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/wundr/keymap.c
@@ -35,12 +35,9 @@ enum custom_keycodes {
 #define CTL_ESC CTL_T(KC_ESC)
 #define SFT_MEH SFT_T(KC_PAUSE)
 #define SFT_MIN MT(MOD_RSFT, KC_MINS)
-#define CTL_J CTL_T(KC_J)
-#define CTL_F CTL_T(KC_F)
-#define SFT_K SFT_T(KC_K)
-#define SFT_D SFT_T(KC_D)
 #define CTL_QT MT(MOD_RCTL, KC_QUOT)
 #define LW_SPC LT(_LW, KC_SPC)
+#define LW_BSP LT(_LW, KC_BSPC)
 #define FGUI GUI_T(KC_F2)
 #define GUIGO GUI_T(KC_ENT)
 
@@ -49,20 +46,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT_5x6(
      KC_GRV,  KC_1, KC_2,    KC_3,    KC_4, KC_5,                      KC_6, KC_7, KC_8,    KC_9,  KC_0,     KC_EQL,
      KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R, KC_T,                      KC_Y, KC_U, KC_I,    KC_O,  KC_P,     KC_BSLS,
-     /* CTL_ESC, KC_A, KC_S,    KC_D,    KC_F, KC_G,                      KC_H, KC_J, KC_K,    KC_L  ,KC_SCLN,  CTL_QT, */
-     CTL_ESC, KC_A, KC_S,    SFT_D,   CTL_F,KC_G,                      KC_H, CTL_J,SFT_K,   KC_L  ,KC_SCLN,  CTL_QT,
+     CTL_ESC, KC_A, KC_S,    KC_D,    KC_F, KC_G,                      KC_H, KC_J, KC_K,    KC_L  ,KC_SCLN,  CTL_QT,
      SFT_MEH, KC_Z, KC_X,    KC_C,    KC_V, KC_B,                      KC_N, KC_M, KC_COMM, KC_DOT ,KC_SLSH, SFT_MIN,
                     KC_LBRC, KC_RBRC,                                              KC_PGUP, KC_PGDN,
                              FGUI,    FN_BSP,                        FN_SPC,  GUIGO,
-                             _______,  LW_SPC,                        KC_LEAD,  M6,
+                             KC_LEAD,  LW_SPC,                       KC_LEAD,  M6,
                              _______, KC_RALT,                       KC_LALT, _______
   ),
 
   [_FN] = LAYOUT_5x6(
-     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-     KC_CAPS, KC_BTN4, KC_BTN3, _______, _______, _______,              KC_HOME, KC_END,  KC_LBRC, KC_RBRC, _______, KC_DEL,
-     _______, M1_S,    M2_S,    M1_R,    M2_R,    M_STOP,               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSLS, _______,
-     _______, M1,      M2,      M3,      M4,      M5,                   KC_PGUP, KC_PGDN, _______,  _______, _______, _______,
+     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,
+     KC_CAPS, KC_BTN4, KC_BTN3, _______, _______, _______,              KC_HOME, KC_END,  KC_LBRC, KC_RBRC,  _______, KC_DEL,
+     _______, M1_S,    M2_S,    M1_R,    M2_R,    M_STOP,               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______, _______,
+     _______, M1,      M2,      M3,      M4,      M5,                   _______, _______, _______,  _______, _______, _______,
                        _______, _______,                                                  _______, _______,
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______,
@@ -110,6 +106,9 @@ void matrix_scan_user(void) {
     SEQ_TWO_KEYS(KC_D, KC_P) {
       SEND_STRING("docker exe"SS_TAP(X_TAB)"-it eva"SS_TAP(X_TAB)"post"SS_TAP(X_TAB)"bash"SS_TAP(X_ENTER)"psql -U postgres -d evalink -p 5132"SS_TAP(X_ENTER)"\\x auto"SS_TAP(X_ENTER));
       /* SEND_STRING(SS_LCTRL("a")SS_LCTRL("c")); */
+    }
+    SEQ_TWO_KEYS(KC_T, KC_P) {
+      SEND_STRING("ssh -L 2050:localhost:5132 ");
     }
     SEQ_THREE_KEYS(KC_D, KC_D, KC_S) {
       SEND_STRING("https://start.duckduckgo.com"SS_TAP(X_ENTER));
