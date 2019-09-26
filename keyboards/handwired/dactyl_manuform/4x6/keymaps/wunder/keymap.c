fe3e30a113223b6cfc628d2d27e3e57a05b29b55
Dmitry Shashkov <shash@mera.ru>
2019-09-26 17:25:31 +0300

    no

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/wunder/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/wunder/keymap.c
@@ -50,12 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT(
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSLS,
      CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, CTL_QT,
-     KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_MINS,
+     SFT_MEH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, SFT_MIN,
                        KC_PGUP, KC_PGDN,                                      KC_LBRC, KC_RBRC,
                                          FGUI,    LW_BSP,            FN_SPC,  GUIGO,
-                                         KC_LEAD, SFT_SPC,           SFT_BSP, M6,
+                                         KC_LEAD, FN_SPC,            LW_BSP, M6,
                                          KC_PAUS, KC_RALT,           KC_LALT, _______
-     /* SFT_MEH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, SFT_MIN, */
                        /* KC_PGDN, KC_PGUP,                                      KC_LBRC, KC_RBRC, */
   ),
 
