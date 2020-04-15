2ab73d190d3bc37eea0fdd8603a628a41a930f97
Wunder Dmitry <i.draenei@gmail.com>
2020-04-15 12:38:35 +0300

    sparkol

--- keyboards/sparkle/keymaps/default/keymap.c
+++ keyboards/sparkle/keymaps/default/keymap.c
@@ -50,11 +50,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QW] = LAYOUT(
-         KC_TAB,  KC_Q, KC_W, KC_E, KC_R,    KC_T,                         KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,    KC_BSLS,
-         CTL_ESC, KC_A, KC_S, KC_D, KC_F,    KC_G,                         KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN, CTL_QT,
- SFT_MEH,         KC_Z, KC_X, KC_C, KC_V,    KC_B,   KC_1,        KC_2,    KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,        GUI_MIN,
-                              FGUI, LW_BSP,  FN_SPC, KC_LALT,     KC_RALT, CTL_BS, FN_SPC, SFTGO,
-                                    KC_LEAD, KC_2,   KC_3,        KC_4,    KC_5,   M6
+         KC_TAB,  KC_Q, KC_W, KC_E, KC_R,    KC_T,                          KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,    KC_BSLS,
+         CTL_ESC, KC_A, KC_S, KC_D, KC_F,    KC_G,                          KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN, CTL_QT,
+ SFT_MEH,         KC_Z, KC_X, KC_C, KC_V,    KC_B,    KC_1,        KC_2,    KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,        GUI_MIN,
+                              FGUI, LW_BSP,  SFT_SPC, KC_LALT,     KC_RALT, CTL_BS, FN_SPC, SFTGO,
+                                    KC_LEAD, FN_SPC,  KC_3,        KC_4,    KC_5,   M6
   ),
 
   [_FN] = LAYOUT(
@@ -152,4 +152,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 };
-
+    
