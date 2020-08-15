89d7bd05759960b2aba768b928a1710515173be2
WundeR Dmitry <wunder@testchamber.one>
2020-08-15 20:35:29 +0300

    more sensible firmware for dz60

--- keyboards/dz60/keymaps/wunder/keymap.c
+++ keyboards/dz60/keymaps/wunder/keymap.c
@@ -69,36 +69,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT_60_ansi_split_bs_rshift(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
     KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-    KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_A,
-    KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          MO(_FN), KC_RCTL
+    CTL_ESC,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_LEAD,
+    KC_LCTL, KC_LGUI,          KC_LALT,                   FN_SPC,                             KC_RALT, KC_RGUI,          MO(_FN), KC_RCTL
     ),
 
   [_FN] = LAYOUT_60_ansi_split_bs_rshift(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DEL,
-    KC_TRNS,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
-    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_HOME, KC_PGUP, KC_TRNS,
-    KC_TRNS,          KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_TRNS, KC_DEL,  KC_END,  KC_PGDN,          KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS
+    _______, KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,
+    _______,         _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______, _______, _______, _______, _______, KC_DEL,
+    _______,         _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
+    _______,         _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,          _______, RESET,
+    _______, _______,         _______,                   _______,                            _______, _______,          _______, _______
     )
 };
 
-//  [_LW] = LAYOUT(
-//         KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-//         KC_GRV,  KC_1,    KC_2,  KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-// KC_F1,           KC_F2,   KC_F3, KC_F4,   KC_F5,   KC_F6, KC_1,          KC_2,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,          KC_F12,
-//                                  _______, _______, _______, _______,     _______, _______, _______, _______,
-//                                           _______, _______, _______,     _______, _______, _______
-//  ),
-//
-//  [_RS] = LAYOUT(
-//         _______, _______, _______, _______, M_STOP,  M1_REC,                        KC_INS,  _______, _______, _______, KC_SLCK, KC_PAUS,
-//         _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, M2_REC,                        _______, _______, _______, _______, _______, _______,
-// RESET,           _______, _______, _______, _______, KC_PSCR, KC_1,        KC_2,    _______, _______, _______, _______, _______,         _______,
-//                                    _______, _______, _______, _______,     _______, _______, _______, _______,
-//                                             _______, _______, _______,     _______, _______, _______
-//  ),
-//};
 
 LEADER_EXTERNS();
 
